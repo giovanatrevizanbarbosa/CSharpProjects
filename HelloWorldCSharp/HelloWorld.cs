bc923e9f15098ae4768b65145f04b674dbedcac4
giovanatrevizanbarbosa <gi.trevizan.barbosa@gmail.com>
2024-03-11 17:09:18 -0300

    setup project and first HelloWorld

---
+++ HelloWorldCSharp/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorldCSharp;
+
+public class HelloWorld
+{
+    private string hello = "";
+
+    public void SetHello(string message)
+    {
+        this.hello = message;
+    }
+
+    public string GetHello()
+    {
+        return hello;
+    }
+}
---
+++ HelloWorldCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorldCSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HelloWorld helloMessage = new HelloWorld();
+        helloMessage.SetHello("Hello World");
+        Console.WriteLine(helloMessage.GetHello());
+    }
+}
